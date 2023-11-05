fff74110ff5e4a9ff58cc71bb8538f207a6eb50d
Arif Hussain <arif3337179@gmail.com>
2023-11-05 22:28:31 +0500

    bootstrap form component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Header from './components/header';
 import HeroSection  from './components/hero-section';
 import Footer from './components/footer';
 import Form from './components/form';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
--- src/components/form.js
+++ src/components/form.js
@@ -1,66 +1,32 @@
-export default function Form() {
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
 
+function BasicExample() {
     return (
-        <div style={formStyle}>
-            <h1>Form</h1>
-            <form >
-                <div>
-                    <label>Name</label>
-                    <input type="text" style={inputStyle} />
-                </div>
-                <div>
-                    <label>Email</label>
-                    <input type="email" style={inputStyle}/>
-                </div>
-                <div>
-                    <label>Message</label>
-                    <textarea style={textareaStyle}></textarea>
-                </div>
-                <button style={buttonStyle}>Submit</button>
-            </form>
-        </div>
+        <Form >
+            <Form.Group style={formStyle} className="mb-3" controlId="formBasicEmail">
+                <Form.Label>Email address</Form.Label>
+                <Form.Control type="email" placeholder="Enter email" />
+            </Form.Group>
+
+            <Form.Group style={formStyle} className="mb-3" controlId="formBasicPassword">
+                <Form.Label>Password</Form.Label>
+                <Form.Control type="password" placeholder="Password" />
+            </Form.Group>
+            <Form.Group style={formStyle} className="mb-3" controlId="formBasicCheckbox">
+                <Form.Check type="checkbox" label="Check me out" />
+            </Form.Group>
+            <Button style={formStyle} variant="primary" type="submit">
+                Submit
+            </Button>
+        </Form>
     );
 }
 
 const formStyle = {
-    padding: '100px 0',
-    marginTop: '50px',
-    marginBottom: '50px',
-    textAlign: 'center',
-    background: '#f4f4f4',
-    
-}
-const inputStyle = {
-    padding: '10px',
-    border: 'none',
-    borderBottom: '2px solid #ccc',
-    width: '100%',
-    marginBottom: '20px',
-    fontSize: '16px',
-    fontFamily: 'sans-serif'
+    alignItems: 'center',
+    justifyContent: 'center',
+    display: 'flex',
 };
 
-const textareaStyle = {
-    padding: '10px',
-    border: 'none',
-    borderBottom: '2px solid #ccc',
-    width: '100%',
-    marginBottom: '20px',
-    fontSize: '16px',
-    fontFamily: 'sans-serif'
-};
-
-
-const buttonStyle = {
-    backgroundColor: '#4CAF50',
-    border: 'none',
-    color: 'white',
-    padding: '15px 32px',
-    textAlign: 'center',
-    textDecoration: 'none',
-    display: 'inline-block',
-    fontSize: '16px',
-    margin: '4px 2px',
-    cursor: 'pointer'
-};
-
+export default BasicExample;
